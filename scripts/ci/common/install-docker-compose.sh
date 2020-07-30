8175c255c3e81da32a380d358ad2369f1df9ecf8
Toby Clemson <tobyclemson@gmail.com>
2020-07-30 20:28:54 +0100

    Use docker-compose package in CI.

--- scripts/ci/common/install-docker-compose.sh
+++ scripts/ci/common/install-docker-compose.sh
@@ -5,9 +5,4 @@ set -e
 set -o pipefail
 
 apk --update add \
-    curl
-
-curl -L \
-    https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` \
-    > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+    docker-compose
