7e0535a21e373b2dfc9745c1b00c766010aace8b
Toby Clemson <tobyclemson@gmail.com>
2020-07-30 20:16:05 +0100

    Install docker compose in CI.

---
+++ scripts/ci/common/install-docker-compose.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+[ -n "$DEBUG" ] && set -x
+set -e
+set -o pipefail
+
+curl -L \
+    https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` \
+    > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
